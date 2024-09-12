0bb411666915a83453300233821e0b5a28df012c
Clinton Thomas <29966176+hayseedbyte@users.noreply.github.com>
2024-09-12 05:06:38 -0400

    It works

--- script.js
+++ script.js
@@ -1,62 +1,103 @@
-import recipes from "./recipes_link2.json" with { type: "json" };
 
 const checkboxes = document.querySelectorAll("input");
-const ingredients = checkboxes.values();
-// console.log(ingredients);
-// console.log(checkboxes);
-// for (let i = 0; i < ingredients.length; i++) {
-//   const checked = ingredients[i] + "-check";
-//   checkboxes.push(checked);
-// }
-// console.log(checkboxes);
-const selection = []
+
+
+const toTitleCase = function(str) {
+  return str.replace(
+    /\w\S*/g,
+    text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+  );
+}
+
 const getIngredients = function (arr) {
   const list = [];
   for (let i = 0; i < arr.length; i++) {
     const element = arr[i];
-    // console.log(element);
     if (element.checked) {
       list.push(element.value);
     }
   }
-  console.log(list);
 
   return list;
 };
-const getRecipes = function (arr) {
+const getRecipes = function (checked) {
   const output = []
   for (let i = 0; i < recipes.length; i++) {
     const r = recipes[i];
-    const x = r.ingredients
-    console.log(x);
-    for (let o = 0; o < arr.length; o++) {
-      const e = arr[o];
-      if (x.name === e) {
-        output.push(recipes[i].name);
+    const ingredients = r.ingredients
+    for (let j = 0; j < ingredients.length; j++){
+      for (let h = 0; h < checked.length; h++){
+        if (checked[h] === ingredients[j].name){
+          output.push(r.name)
+        }
       }
     }
+    
+
+    
   }
-  console.log(output);
   return output;
 };
 
-// const deselect = function (e) {
-//   const nodeMap = e.attributes;
-//   const name = nodeMap[3].value;
-//   e.style.backgroundColor = "darkgrey";
-//   selection.pop(name);
-// };
-// const item = document.getElementsByClassName("item");
+const itemCounter = (array, item) => {
+  return array.filter((currentItem) => currentItem == item).length;
+};
+const injectHTML = function (arr){
+  const htmlArray = []
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    const name = element.name
+    const undashed = element.name.replace('-',' ');
+    const title = toTitleCase(undashed);
+    const image = element.image;
+    const ingredients = element.ingredients
+    // for (let j = 0; j < ingredients.length; j++){
+
+    // }
+    const html = `${title}<br><img alt="${name}" class="item-img" src="${image}"/>`
+    htmlArray.push(html)
+  
+  }
+  return htmlArray;
+}
+
 const button = document.getElementById("show");
-button.addEventListener("click", function () {
-  console.log("clicked");
-  const selection = getIngredients(checkboxes);
-  console.log(selection);
-  console.log('selection');
-  const output = getRecipes(selection);
-  // for (let i = 0; i < checkboxes.length; i++) {
-  //   const element = checkboxes[i];
-  //   element.checked = false;
+button.addEventListener("click", function (e) {
+  e.preventDefault()
+  const list = getIngredients(checkboxes)
+  const result = getRecipes(list)
+  const final = [];
+  for (let i = 0; i < checkboxes.length; i++) {
+    const element = checkboxes[i];
+    element.checked = false;
+  }
+
+ 
+  for (let i =0;i<result.length;i++){
+    if(itemCounter(result, result[i]) >= list.length)
+      final.push(result[i]);
+  }
+  const showRecipes = []
+  const finalSet = new Set(final);
+  console.log(finalSet)
+  const a = finalSet.values()
+  for (const e of finalSet) {
+    for (let j = 0; j < recipes.length; j++){
+      if (e === recipes[j].name){
+        showRecipes.push(recipes[j])
+      }
+  
+  }
+  // for (let i = 0; i < finalSet.length; i++){
+  //     for (let j = 0; j < recipes.length; j++){
+  //       if (finalSet[i] === recipes[j].name){
+  //         console.log(recipes[j])
+  //         showRecipes.push(recipes[j])
+  //       }
+  //     }
   // }
-  console.log(output);
-});
+  console.log(showRecipes)
+  const resultsDiv = document.querySelector('.show-recipes');
+  const resultsHTML = injectHTML(showRecipes);
+  resultsDiv.innerHTML = resultsHTML.join("<br>");
+}});
